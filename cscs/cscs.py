c975d04946c140f88f30dcb2b2a0d2f84b31afb7
shr264 <shr264@ufl.edu>
2017-01-22 03:58:18 -0500

    0122

--- cscs/cscs.py
+++ cscs/cscs.py
@@ -1,4 +1,4 @@
-#### This is the CSCS algorithm by Khare, Oh, Rahman and Rajaratnam. The code was written by Syed Rahman. Please look at accompanying file for an example of how to use
+#### This is the CSCS algorithm by Khare, Oh, Rahman and Rajaratnam. Please look at accompanying file for an example of how to use
 
 from __future__ import division
 import math
@@ -37,7 +37,7 @@ def cscs(Y,lmbda,initOm,maxitr,eps):
     L = numpy.identity(p)
     L[0,0] = 1/math.sqrt(S[0,0])
     for i in range(1,p):
-        L[i,numpy.arange(0,i+1)] = h(i,S[0:i+1,0:i+1],lmbda,initOm[i,numpy.arange(0,i+1)],maxitr,eps)
+        L[i,numpy.arange(0,i+1)] = h(i,S[0:(i+1),0:(i+1)],lmbda,initOm[i,numpy.arange(0,i+1)],maxitr,eps)
     return L
 
 
--- cscs/cscstest.py
+++ cscs/cscstest.py
@@ -4,11 +4,13 @@ import numpy
 from scipy import random, linalg
 from cscs import cscs
 
-
+random.seed(5)
 p = 5
-n = 7
+n = 10
 A = random.rand(p,p)
-Sigma = numpy.dot(A,A.transpose())
+b = .5*A.max()
+A[A<b]=0
+Sigma = numpy.dot(A.transpose(),A)
 mu = numpy.zeros(p)
 Y = numpy.random.multivariate_normal(mu,Sigma,n)
-cscs(Y,2,numpy.identity(p),100,0.0001)
+cscs(Y,20,numpy.identity(p),100,0.0001)
