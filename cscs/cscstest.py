18c45cecdf206311ef7aa34d76098cab61668065
shr264 <shr264@ufl.edu>
2017-01-29 19:16:38 -0500

    graph

--- cscs/cscstest.py
+++ cscs/cscstest.py
@@ -1,6 +1,9 @@
 from __future__ import division
+import pylab
+import networkx as nx
 import math
-import numpy
+import matplotlib
+import numpy as np
 from scipy import random, linalg
 from cscs import cscs
 
@@ -10,7 +13,13 @@ n = 10
 A = random.rand(p,p)
 b = .5*A.max()
 A[A<b]=0
-Sigma = numpy.dot(A.transpose(),A)
-mu = numpy.zeros(p)
-Y = numpy.random.multivariate_normal(mu,Sigma,n)
-cscs(Y,20,numpy.identity(p),100,0.0001)
+Sigma = np.dot(A.transpose(),A)
+mu = np.zeros(p)
+Y = np.random.multivariate_normal(mu,Sigma,n)
+L = cscs(Y,50,np.identity(p),100,0.0001)
+L[L<0.001] = 0
+adj = abs(L)>0
+adj = adj.astype(int)
+G = nx.Graph(adj)
+nx.draw(G, with_labels=True)
+pylab.show()
